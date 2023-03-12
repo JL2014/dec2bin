bef5e8db7dded05d6f31d4f718c6ca6ed72a6ff4
Jean Louis <JL2014@users.noreply.github.com>
2023-03-12 20:31:13 +0100

    Update dec2bin.cpp

--- dec2bin.cpp
+++ dec2bin.cpp
@@ -4,16 +4,18 @@
 
 using namespace std;
 
+void validate(string n) {
+    if(n.length() == 0) throw 2; // empty parameter case : ""
+    else if(n.length()>0 && n[0] == '-') throw 3;
+    for (int i = 0; i < n.length(); i++) {
+        if(!isdigit(n[i])) throw 4; // invalid cases : "+2" or "0xA" or "1,2" or "3.4" or "5.6e+07" ...
+    }
+}
+
 string dec2bin(string n) {    
     mpz_class mpz_n;
-    if(n.length() == 0) throw 2; // empty parameter case : ""
-    for (int i = 0; i < n.length(); i++) {
-        if(i == 0 && n[i] == '-' && n.length()>1) continue;
-        if(!isdigit(n[i])) throw 3; // invalid cases : "-" or "0xA" or "1,2" or "3.4" or "5.6e+07" ...
-    }
-    mpz_n.set_str(n, 10); // used instead of mpz_n(n) to avoid octal notation, i.e. numbers starting with one or more zeros "016" ...
+    mpz_n.set_str(n, 10); // used instead of mpz_n(n) to avoid octal notation, i.e. numbers starting with one or more zeros : "016" ...
     if(mpz_n == 0) return "0"; // zero cases : "0" or "00" ...
-    if(mpz_n < 0) throw 4;
     n = "";
     while (mpz_n > 0) {
         n += (mpz_n % 2) == 0 ? "0" : "1";
@@ -27,6 +29,7 @@ int main(int argc, char *argv[]) {
     try
     {
         if(argc > 1) {
+            validate(argv[1]);
             cout << dec2bin(argv[1]) << endl;
         } else {
             throw 1;
